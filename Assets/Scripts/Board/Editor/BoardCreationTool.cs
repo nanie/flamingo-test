edd0fe73c2ed372bdd7ca8ab75fd02ecb4f88ecc
Camilla Camatta <camillacamatta@gmail.com>
2025-01-12 00:54:15 +0100

    Add first version of map creation tool

---
+++ Assets/Scripts/Board/Editor/BoardCreationTool.cs
@@ -0,0 +1,177 @@
+using Flamingo.Board;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public class BoardCreationTool : EditorWindow
+{
+    private static bool _isEditing = false;
+    private static List<Board.Tile> _tiles;
+    private static Vector3 cubePosition;
+    private const float TILE_SIZE = 0.5f;
+    private static Vector3 deltaMove = Vector3.zero;
+    private void OnEnable()
+    {
+        cubePosition = Vector3.zero;
+    }
+
+    static BoardCreationTool()
+    {
+        SceneView.duringSceneGui += OnSceneGUI;
+    }
+
+    private static void OnSceneGUI(SceneView sceneView)
+    {
+        if (_isEditing)
+        {
+            Vector3 currentPosition = Vector3.zero;
+            foreach (var item in _tiles)
+            {
+                DrawCube(currentPosition);
+                currentPosition = GetPosition(item.Next, currentPosition, TILE_SIZE * 2);
+            }
+            DrawMoveHandle();
+            DrawEditing(sceneView);
+        }
+        else
+        {
+            DrawDefault(sceneView);
+        }
+    }
+
+    private static Vector3 GetPosition(Board.Direction direction, Vector3 position, float distance)
+    {
+        return direction switch
+        {
+            Board.Direction.Left => position + new Vector3(0, 0, distance),
+            Board.Direction.Right => position + new Vector3(0, 0, -distance),
+            Board.Direction.Front => position + new Vector3(-distance, 0, 0),
+            Board.Direction.Back => position + new Vector3(distance, 0, 0),
+            _ => Vector3.zero
+        };
+    }
+
+    private static void DrawDefault(SceneView sceneView)
+    {
+        Handles.BeginGUI();
+        DrawButton(StartMapCreation, "Create map", sceneView, 0);
+        Handles.EndGUI();
+    }
+    private static void DrawEditing(SceneView sceneView)
+    {
+        Handles.BeginGUI();
+        DrawButton(ExportMap, "Export map", sceneView, 0);
+        DrawButton(() => { _isEditing = false; }, "Cancel", sceneView, 1);
+        Handles.EndGUI();
+    }
+
+    private static void DrawButton(Action onClick, string label, SceneView sceneView, int index)
+    {
+        var buttonWidth = 100;
+        var buttonHeight = 30;
+        var padding = 10;
+        var buttonRect = new Rect(
+            sceneView.position.width - buttonWidth - padding,
+            sceneView.position.height - buttonHeight * 2 - (index * buttonHeight) - padding - (padding * index),
+            buttonWidth,
+            buttonHeight
+        );
+        if (GUI.Button(buttonRect, label))
+        {
+            onClick();
+        }
+    }
+    private static void StartMapCreation()
+    {
+        _isEditing = true;
+        deltaMove = Vector3.zero;
+        cubePosition = Vector3.zero;
+        _tiles = new List<Board.Tile>();
+        _tiles.Add(new Board.Tile { });
+    }
+
+    public static void DrawCube(Vector3 position)
+    {
+        Handles.color = Color.yellow;
+        if (Event.current.type == EventType.Repaint)
+        {
+            Handles.CubeHandleCap(
+                  0,
+                  position,
+                  Quaternion.identity,
+                  TILE_SIZE,
+                  EventType.Repaint
+                  );
+        }
+    }
+
+    public static void DrawMoveHandle()
+    {
+        var newPosition = SnapToGrid(Handles.PositionHandle(cubePosition, Quaternion.identity));
+        if (newPosition != Vector3.zero)
+        {
+            deltaMove += cubePosition - newPosition;
+            cubePosition = newPosition;
+
+            if (deltaMove.magnitude < (TILE_SIZE + 0.2F))
+            {
+                return;
+            }
+
+            int tilesToAdd = 0;
+            Board.Direction direction = Board.Direction.Front;
+
+            if (Mathf.Abs(deltaMove.x) > Mathf.Abs(deltaMove.z))
+            {
+                direction = deltaMove.x > 0 ? Board.Direction.Front : Board.Direction.Back;
+                tilesToAdd = Mathf.FloorToInt(Mathf.Abs(deltaMove.x) / (TILE_SIZE + 0.2F));
+            }
+            else
+            {
+                direction = deltaMove.z < 0 ? Board.Direction.Left : Board.Direction.Right;
+                tilesToAdd = Mathf.FloorToInt(Mathf.Abs(deltaMove.z) / (TILE_SIZE + 0.2F));
+            }
+
+            AddTiles(tilesToAdd, direction);
+            deltaMove = Vector3.zero;
+        }
+    }
+
+    private static void AddTiles(int tilesToAdd, Board.Direction direction)
+    {
+        for (int i = 0; i < tilesToAdd; i++)
+        {
+            Board.Tile newTile = new Board.Tile
+            {
+                Next = direction,
+            };
+            _tiles.Add(newTile);
+        }
+    }
+    private static Vector3 SnapToGrid(Vector3 position)
+    {
+        return new Vector3(
+            Mathf.Round(position.x / TILE_SIZE) * TILE_SIZE,
+            position.y,
+            Mathf.Round(position.z / TILE_SIZE) * TILE_SIZE
+        );
+    }
+    private static void ExportMap()
+    {
+        Board board = new Board { Tiles = _tiles.ToArray() };
+        string json = JsonConvert.SerializeObject(board, Formatting.Indented);
+
+        string path = EditorUtility.SaveFilePanel("Save Map", "", "newMap.json", "json");
+
+        if (!string.IsNullOrEmpty(path))
+        {
+            File.WriteAllText(path, json);
+            Debug.Log($"Map saved to: {path}");
+        }
+    }
+
+}
